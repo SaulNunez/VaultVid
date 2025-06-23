c9606a14ad0d112b603cb1e36d6ecc454e12958a
Saul Nunez <saul.nunez99@gmail.com>
2025-06-23 15:24:06 -0600

    Change names of bucket field and change Public ID to a GUID

--- Models/Video.cs
+++ Models/Video.cs
@@ -3,10 +3,10 @@ namespace VideoHostingService.Models;
 public class Video
 {
     public int Id { get; set; }
-    public string PublicId { get; set; }
+    public Guid PublicId { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
-    public string Bucket { get; set; }
+    public string ObjectName { get; set; }
     public string Tags { get; set; }
     public string ThumbnailLocation { get; set; }
     public DateTimeOffset CreatedAt { get; set; }
